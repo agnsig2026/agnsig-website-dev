d0af16abd9451a508e8434139b44d424b426d9e6
Ming-Yi <linmy2016@gmail.com>
2026-03-18 21:19:10 -0400

    add talks in talksData.tsx

--- src/components/Talks/talksData.tsx
+++ src/components/Talks/talksData.tsx
@@ -5,7 +5,23 @@ import { Talk } from "@/types/talk";
 
 const talksData: Talk[] = [
   {
-    id: 4,
+    id: 9,
+    type: "Spotlight",
+    date: "17 Mar 2026",
+    name: "Dr. Dominika Krol \n(Center for Astrophysics)",
+    title: "AGN-driven metallicity enrichment in the ISM of Mrk 573",
+    url: "https://www.youtube.com/watch?v=h3f_-hObxBk&t=0s" 
+  },
+  {
+    id: 8,
+    type: "Spotlight",
+    date: "17 Mar 2026",
+    name: "Dr. Justin Kader \n(Univ. of California Irvine) ",
+    title: "The Past, Present, and Future of a Precessing Jet-driven Outflow in a Late-type Disk Galaxy",
+    url: "https://www.youtube.com/watch?v=h3f_-hObxBk&t=1679s" 
+  },
+  {
+    id: 7,
     type: "Vision",
     date: "17 Feb 2026",
     name: "Dr. Santiago Garcia-Burillo \n(Observatorio Astronómico Nacional)",
@@ -13,7 +29,7 @@ const talksData: Talk[] = [
     url: "https://youtu.be/9CH5QxFyXm8" 
   },
   {
-    id: 3,
+    id: 6,
     type: "Spotlight",
     date: "27 Jan 2026",
     name: "Dr. Namrata Roy \n(Arizona State University)",
@@ -21,12 +37,36 @@ const talksData: Talk[] = [
     url: "https://www.youtube.com/watch?v=Hy_y2V-ODFo&t=209s" 
   },
   {
-    id: 2,
+    id: 5,
     type: "Spotlight",
     date: "27 Jan 2026",
     name: "Dr. Lulu Zhang \n(University of Texas, San Antonio)",
     title: "New Insights into AGN Feedback in the Local Universe: A Spatially Resolved View from JWST Spectroscopy",
     url: "https://www.youtube.com/watch?v=Hy_y2V-ODFo&t=1891s" 
+  },
+  {
+    id: 4,
+    type: "AAS247 Splinter",
+    date: "06 Jan 2026",
+    name: "Dr. Jianwei Lyu \n(University of Arizona)",
+    title: "Unveiling the High-redshift Obscured AGN Population with JWST/MIRI",
+    url: "https://www.youtube.com/watch?v=tcjcMlWlBOM&t=2902s" 
+  },
+  {
+    id: 3,
+    type: "AAS247 Splinter",
+    date: "06 Jan 2026",
+    name: "Dr. Tiffany Lewis \n(Michigan Technological University)",
+    title: "The Role of Gamma Rays in Elucidating Active Galaxies",
+    url: "https://www.youtube.com/watch?v=tcjcMlWlBOM&t=1963s" 
+  },
+  {
+    id: 2,
+    type: "AAS247 Splinter",
+    date: "06 Jan 2026",
+    name: "Dr. Mitchell Revalski \n(STScI)",
+    title: "Unlocking Precise AGN Outflow Energetics with Spatially Resolved Modeling",
+    url: "https://www.youtube.com/watch?v=tcjcMlWlBOM&t=934s" 
   },
   {
     id: 1,
